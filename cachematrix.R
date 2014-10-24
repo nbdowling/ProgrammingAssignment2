e922e2e6bfaf53b99bcca4218c40987f113ccb76
nbdowling <nbdowling@users.noreply.github.com>
2014-10-24 08:07:39 -0400

    Began filling out cachematrix.r
    1) added comments
    2) copied makeVector & cacheMean  from Problem Statement as examples

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions use the same general form as makeVector & cacheMean in the
+## problem statement.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions to set/get the matrix & its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
 
-## Write a short comment describing this function
+## cacheSolve inverts the matrix or returns the inverse from the cache (if has already
+## been calculated)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
