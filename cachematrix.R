5ffde262d65a20ab338521b66a56549c84be8262
nbdowling <nbdowling@users.noreply.github.com>
2014-10-24 15:22:37 -0400

    Populated makeCacheMatrix & cacheSolve w/ code
    1) copied body of makeVector to makeCacheMatrix and body of cacheMean to cacheSolve
    2) changed occurrences of "mean" to "inverse"
    3) revised comments
    4) removed makeVector & cacheMean

--- cachematrix.R
+++ cachematrix.R
@@ -4,38 +4,31 @@
 ## makeCacheMatrix creates a list of functions to set/get the matrix & its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-makeVector <- function(x = numeric()) {
         m <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
         list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-## cacheSolve inverts the matrix or returns the inverse from the cache (if has already
+## cacheSolve inverts the matrix or returns the inverse from the cache (if it has already
 ## been calculated)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
-
-cachemean <- function(x, ...) {
-        m <- x$getmean()
+        m <- x$getinverse()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
         data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
+        m <- inverse(data, ...)
+        x$setinverse(m)
         m
-}
+}}
